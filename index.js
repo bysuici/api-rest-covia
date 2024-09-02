1e655b0f56a3a30bc6ab46cd98290d630d77f370
decidesuici <eduardx62@outlook.com>
2024-09-02 15:54:42 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express, { urlencoded, json } from 'express'
+import cors from 'cors'
+import { router } from './routes/routes'
+
+const app = express()
+const port = 3000
+
+// Configuracion del servicio
+app.use(urlencoded({ extended: true }))
+app.use(json())
+app.use(cors())
+
+// Routers
+app.use(router)
+
+// Configurar puerto
+app.listen(port, () => {
+    console.log(`Listen on port 3000`)
+})
