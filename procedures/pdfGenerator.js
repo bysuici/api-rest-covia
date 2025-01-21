ec8da07f2be6eb6c850399d70284a2a5ca50f861
angelzd27 <angeltj27@gmail.com>
2025-01-21 10:05:33 -0600

    colors in alerts

--- procedures/pdfGenerator.js
+++ procedures/pdfGenerator.js
@@ -9,6 +9,146 @@ export const pdfGenerator = async (device, from, to) => {
         format: 'A4',
         printBackground: true
     }
+    const alerts = [
+        {
+            id: 1,
+            alert: "Alerta de pérdida de video",
+            color: "#000000",
+            status: true
+        },
+        {
+            id: 3,
+            alert: "Alerta de cobertura de cámara",
+            color: "#FF0000",
+            status: true
+        },
+        {
+            id: 13,
+            alert: "Alerta de Emergencia",
+            color: "#00FF99",
+            status: true
+        },
+        {
+            id: 18,
+            alert: "Alerta de geo-cercado",
+            color: "#005005",
+            status: true
+        },
+        {
+            id: 19,
+            alert: "Apagado ilegal",
+            color: "#FFC6DE",
+            status: true
+        },
+        {
+            id: 20,
+            alert: "Apagado forzado",
+            color: "#FF00FF",
+            status: true
+        },
+        {
+            id: 29,
+            alert: "Alerta de temperatura",
+            color: "#00A8CD",
+            status: true
+        },
+        {
+            id: 36,
+            alert: "Alerta de distancia",
+            color: "#A0A0A0",
+            status: true
+        },
+        {
+            id: 38,
+            alert: "Alerta de cambio de carril",
+            color: "#F5FF70",
+            status: true
+        },
+        {
+            id: 47,
+            alert: "Alerta de cambios anormales de temperatura",
+            color: "#0033FF",
+            status: true
+        },
+        {
+            id: 58,
+            alert: "Fatiga de conductor",
+            color: "#9A00FF",
+            status: true
+        },
+        {
+            id: 60,
+            alert: "Detección de teléfono",
+            color: "#3283FF",
+            status: true
+        },
+        {
+            id: 61,
+            alert: "Detección de conductor fumando",
+            color: "#3D4040",
+            status: true
+        },
+        {
+            id: 62,
+            alert: "Detección de conductor distraído",
+            color: "#FFFF00",
+            status: true
+        },
+        {
+            id: 63,
+            alert: "Salida de carril",
+            color: "#FF7F00",
+            status: true
+        },
+        {
+            id: 64,
+            alert: "Aviso de colisión frontal",
+            color: "#695141",
+            status: true
+        },
+        {
+            id: 74,
+            alert: "Alerta de arranque anormal",
+            color: "#03737E",
+            status: true
+        },
+        {
+            id: 160,
+            alert: "Alerta de exceso de velocidad",
+            color: "#ACCDFF",
+            status: true
+        },
+        {
+            id: 162,
+            alert: "Control de la distancia de seguimiento",
+            color: "#00FF00",
+            status: true
+        },
+        {
+            id: 163,
+            alert: "Advertencia de colisión de peatones",
+            color: "#630000",
+            status: true
+        },
+        {
+            id: 164,
+            alert: "Detección de bostezos",
+            color: "#380055",
+            status: true
+        },
+        {
+            id: 169,
+            alert: "Detección de cinturón de seguridad",
+            color: "#E4B7FF",
+            status: true
+        },
+        {
+            id: 392,
+            alert: "Zona ciega",
+            color: "#C54200",
+            status: true
+        }
+    ]
 
     htmlPDF.setOptions(options)
 
@@ -125,8 +265,12 @@ export const pdfGenerator = async (device, from, to) => {
                     colors: [
                         ${device.alerts
                             .filter(alert => alert.value > 0)
-                            .map(alert => `am5.color(0x${alert.color.slice(1)})`)
-                            .join(',')} // Une los colores en un solo string
+                            .map(deviceAlert => {
+                                const matchedAlert = alerts.find(a => a.alert === deviceAlert.category);
+                                return matchedAlert ? `am5.color(0x${matchedAlert.color.slice(1)})` : `am5.color(0x${deviceAlert.color.slice(1)})`;
+                            })
+                            .join(',')
+                        } // Une los colores en un solo string
                     ]
                 });
 
