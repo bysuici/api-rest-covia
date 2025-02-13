57eaff7091fa7f5fcf527df340153d9b79cf031a
angelzd27 <angeltj27@gmail.com>
2025-02-13 17:37:49 -0600

    multer

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { report } from '../controller/controller.js'
+import multer from 'multer'
 
 export const router = Router()
+const upload = multer();
 
 router.post('/report', report)
-router.post('/alarm', (request, response) => {
-    console.log("🚨 Alerta recibida:", request);
-    response.status(200).json({ msg: 'alert_received' })
-})
+router.post('/alarm', upload.none(), (request, response) => {
+    console.log("🚨 Alerta recibida:", request.body); // Ahora debería mostrar los datos correctamente
+
+    response.status(200).json({ msg: 'alert_received' });
+});
